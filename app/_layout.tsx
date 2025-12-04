b6dd91675e563b92f7000747f393d15791641f5e
Riku <riku.mikkonen@tuni.fi>
2025-12-04 13:03:38 +0200

    feat: add Settings screen and improve navigation flow

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,24 @@
-import { Stack } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
 
 export default function RootLayout() {
-	return <Stack />;
+	return (
+		<Tabs
+			screenOptions={({ route }) => ({
+				tabBarIcon: ({ color, size }) => {
+					let iconName: keyof typeof Ionicons.glyphMap = "home";
+					
+					if (route.name === "index") {
+						iconName = "home";
+					} else if (route.name === "Settings") {
+						iconName = "settings";
+					}
+					return <Ionicons name={iconName} size={size} color={color} />;
+				},
+			})}
+		>
+			<Tabs.Screen name="index" options={{ title: "Koti" }} />
+			<Tabs.Screen name="Settings" options={{ title: "Asetukset" }} />
+		</Tabs>
+	);
 }
